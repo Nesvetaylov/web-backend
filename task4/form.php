3730715d31140f445f2c499cc15543b5c95cc563
artem9ev <zemla357@gmail.com>
2024-04-12 19:22:38 +0300

    added cookies to most of fields

--- task4/form.php
+++ task4/form.php
@@ -1,37 +1,98 @@
 <html>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="formStyle.css">
+  <title>task 4</title>
   <head>
     <style>
-/* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
-.error {
-  border: 2px solid red;
-}
+    /* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+    .error {
+      border: 2px solid red;
+    }
     </style>
   </head>
   <body>
-
-<?php
-if (!empty($messages)) {
-  print('<div id="messages">');
-  // Выводим все сообщения.
-  foreach ($messages as $message) {
-    print($message);
-  }
-  print('</div>');
-}
-?>
-
-    <form action="" method="POST">
+    <?php
+    if (!empty($messages)) {
+      print('<div id="messages">');
+      // Выводим все сообщения.
+      foreach ($messages as $message) {
+        print($message);
+      }
+      print('</div>');
+    }
+    ?>
+    <form action="index.php" method="POST" id="svyaz">
+      <h3> Для обратной связи оставьте свои данные: </h3>
       <label>
-        <strong> Фамилия имя отчество:</strong>
-        <br>
-        <input name="fio" type="text" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" placeholder="ФИО" />
+          <strong> Фамилия имя отчество:</strong>
+          <br>
+          <input name="fio" type="text"
+          <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" placeholder="ФИО" />
+      </label>
+      <br>
+      <label>
+          <strong>Номер телефона: </strong>
+          <br>
+          <input name="phone" type="tel" pattern="\+7\-[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}"
+          <?php if ($errors['phone']) {print 'class="error"';} ?> value="<?php print $values['phone']; ?>" placeholder="+7(___)___-__-__" />
+      </label>
+      <br>
+      <label>
+          <strong> Введите вашу почту:</strong>
+          <br>
+          <input name="email" type="email"
+          <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" placeholder="email" />
+      </label>
+      <br>
+      <label>
+          <strong> Дата рождения:</strong>
+          <br>
+          <input name="birthdate" value="" type="date"
+          <?php if ($errors['birthdate']) {print 'class="error"';} ?> value="<?php print $values['birthdate']; ?>" />
+      </label>
+      <br>
+      <strong> Пол:</strong>
+      <label>
+          <input type="radio" name="gender" required value="male">
+          Мужской
       </label>
       <label>
-        <strong> Введите вашу почту:</strong>
-        <br>
-        <input name="email" type="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" placeholder="email" />
+          <input type="radio" name="gender" required value="female">
+          Женский
       </label>
-      <input type="submit" value="ok" />
-    </form>
+      <br>
+      <label>
+          <strong>Любимый язык программирования:</strong>
+          <br />
+          <select name="selections[]" multiple="multiple" width="400"
+          <?php if ($errors['selections']) {print 'class="error"';} ?> value="<?php print $values['selections']; ?>">
+              <option value="lua"> Lua</option>
+              <option value="c"> C</option>
+              <option value="c++"> C++</option>
+              <option value="c#"> C#</option>
+              <option value="php"> PHP</option>
+              <option value="python"> Python</option>
+              <option value="java"> Java</option>
+              <option value="js"> JavaScript</option>
+              <option value="ruby"> Ruby</option>
+              <option value="go"> Go</option>
+          </select>
+      </label>
+      <br>
+      <label>
+          <strong> Биография:</strong>
+          <br>
+          <textarea name="biography" placeholder="Я был писателем, пока не... " 
+          <?php if ($errors['biography']) {print 'class="error"';} ?> value="<?php print $values['biography']; ?>"></textarea>
+      </label>
+      <br>
+      <label>
+          <input type="checkbox" name="check" 
+          <?php if ($errors['checkbox']) {print 'class="error"';} ?> value="<?php print $values['checkbox']; ?>"/>
+          c контрактом ознакомлен(а)
+      </label>
+      <br>
+      <input type="submit" value="Сохранить" />
+  </form>
   </body>
 </html>
--- task4/index.php
+++ task4/index.php
@@ -16,24 +16,66 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   // Складываем признак ошибок в массив.
   $errors = array();
   $errors['fio'] = !empty($_COOKIE['fio_error']);
-  // TODO: аналогично все поля.
+  $errors['phone'] = !empty($_COOKIE['phone_error']);
+  $errors['email'] = !empty($_COOKIE['email_error']);
+  $errors['birthdate'] = !empty($_COOKIE['birthdate_error']);
+  $errors['selections'] = !empty($_COOKIE['selections_error']);
+  $errors['biography'] = !empty($_COOKIE['biography_error']);
+  $errors['checkbox'] = !empty($_COOKIE['checkbox_error']);
 
   // Выдаем сообщения об ошибках.
   if ($errors['fio']) {
     setcookie('fio_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('fio_value', '', 100000);
     $messages[] = '<div class="error">Заполните имя.</div>';
   }
-  // TODO: тут выдать сообщения об ошибках в других полях.
+  if ($errors['phone']) {
+    setcookie('phone_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('phone_value', '', 100000); 
+    $messages[] = '<div class="error">Введите номер телефона.</div>';
+  }
+  if ($errors['email']) {
+    setcookie('email_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('email_value', '', 100000);
+    $messages[] = '<div class="error">Заполните email.</div>';
+  }
+  if ($errors['birthdate']) {
+    setcookie('birthdate_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('birthdate_value', '', 100000);
+    $messages[] = '<div class="error">Заполните дату рождения.</div>';
+  }
+  if ($errors['selections']) {
+    setcookie('selections_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('selections_value', '', 100000);
+    $messages[] = '<div class="error">Выберете интересующие вас языки программирования.</div>';
+  }
+  if ($errors['biography']) {
+    setcookie('biography_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('biography_value', '', 100000);
+    $messages[] = '<div class="error">Заполните биографию.</div>';
+  }
+  if ($errors['checkbox']) {
+    setcookie('checkbox_error', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('checkbox_value', '', 100000);
+    $messages[] = '<div class="error">Укажите согласие на обработку и хранение персональных данных.</div>';
+  }
 
   $values = array(); // Складываем предыдущие значения полей в массив, если есть.
   $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
-  // TODO: аналогично все поля.
+  $values['phone'] = empty($_COOKIE['phone_value']) ? '' : $_COOKIE['phone_value'];
+  $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+  $values['birthdate'] = empty($_COOKIE['birthdate_value']) ? '' : $_COOKIE['birthdate_value'];
+  $values['selections'] = empty($_COOKIE['selections_value']) ? '' : $_COOKIE['selections_value'];
+  $values['biography'] = empty($_COOKIE['biography_value']) ? '' : $_COOKIE['biography_value'];
+  $values['checkbox'] = empty($_COOKIE['checkbox_value']) ? '' : $_COOKIE['checkbox_value'];
 
   include('form.php');
 }
 // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-else {
+else 
+{
   $errors = FALSE; // Проверяем ошибки.
+  // fio
   if (empty($_POST['fio'])) {
     setcookie('fio_error', '1', time() + 24 * 60 * 60); // Выдаем куку на день с флажком об ошибке в поле fio.
     $errors = TRUE;
@@ -41,11 +83,55 @@ else {
   else {
     setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60); // Сохраняем ранее введенное в форму значение на месяц.
   }
-
-// *************
-// TODO: тут необходимо проверить правильность заполнения всех остальных полей.
-// Сохранить в Cookie признаки ошибок и значения полей.
-// *************
+  // phone
+  if (empty($_POST['phone'])) {
+    setcookie('phone_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('phone_value', $_POST['phone'], time() + 30 * 24 * 60 * 60);
+  }
+  // email
+  if (empty($_POST['email'])) {
+    setcookie('email_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+  }
+  // birthdate
+  if (empty($_POST['birthdate'])) {
+    setcookie('birthdate_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('birthdate_value', $_POST['birthdate'], time() + 30 * 24 * 60 * 60);
+  }
+  // biography
+  if (empty($_POST['biography'])) {
+    setcookie('biography_error', '1', time() + 24 * 60 * 60); 
+    $errors = TRUE;
+  }
+  else {
+    setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
+  }
+  // checkbox
+  if (empty($_POST['checkbox'])) {
+    setcookie('checkbox_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('checkbox_value', $_POST['checkbox'], time() + 30 * 24 * 60 * 60);
+  }
+  // selections
+  if (empty($_POST['checkbox'])) {
+    setcookie('checkbox_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('checkbox_value', $_POST['checkbox'], time() + 30 * 24 * 60 * 60);
+  }
+  // TODO: selection, radio
 
   if ($errors) {
     header('Location: index.php'); // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
@@ -53,7 +139,12 @@ else {
   }
   else {
     setcookie('fio_error', '', 100000); // Удаляем Cookies с признаками ошибок.
-    // TODO: тут необходимо удалить остальные Cookies.
+    setcookie('phone_error', '', 100000);
+    setcookie('email_error', '', 100000);
+    setcookie('birthdate_error', '', 100000);
+    setcookie('selections_error', '', 100000);
+    setcookie('biography_error', '', 100000);
+    setcookie('checkbox_error', '', 100000);
   }
 
   // Сохранение в БД.
